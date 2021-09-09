48e3db2a9e0c57d2b2d98671c547ddb482809e59
William-Arthur Cinq-Mars <will_arth53@hotmail.com>
2021-09-09 16:04:04 -0400

    Début programme

---
+++ logiciel_gestion/logiciel_gestion/DB/ConnectionDB.vb
@@ -0,0 +1,11 @@
+﻿Public Class ConnectionDB
+    Public connectionString = "Server='localhost';Database='5_mars_vr';Uid='root';Pwd='';Port=3306;SslMode=none"
+    Shared instance As ConnectionDB = Nothing
+
+    Public Shared Function getInstance() As ConnectionDB
+        If IsNothing(instance) Then
+            instance = New ConnectionDB()
+        End If
+        Return instance
+    End Function
+End Class
---
+++ logiciel_gestion/logiciel_gestion/MainForm.vb
@@ -0,0 +1,26 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class MainForm
+
+    Dim connection As New MySqlConnection(ConnectionDB.getInstance.connectionString)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            If connection.State = ConnectionState.Open Then
+                connection.Close()
+            End If
+            Dim command As New MySqlCommand
+            command.Connection = connection
+            command.CommandText = $"Select * from inventaire"
+            connection.Open()
+            Dim reader = command.ExecuteReader()
+            Dim table As New DataTable("inventaire")
+            table.Load(reader)
+            connection.Close()
+            DataGridView1.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+End Class
