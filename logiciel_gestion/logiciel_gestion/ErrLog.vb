7c209d16368b59877c49bfdb70479c53fe4e131f
William-Arthur Cinq-Mars <will_arth53@hotmail.com>
2021-09-10 13:46:55 -0400

    Ajout piece pour la reception
    La création de l'ajout de piece pour la reception est bien avancé. Il est dans le main forme pour l'instant. Retirer des pieces de l'inventaire n'est pas encore fait.

---
+++ logiciel_gestion/logiciel_gestion/ErrLog.vb
@@ -0,0 +1,24 @@
+﻿Public Class ErrLog
+    Shared instance As ErrLog = Nothing
+
+    Public Shared Function getInstance() As ErrLog
+        If IsNothing(instance) Then
+            instance = New ErrLog
+        End If
+        Return instance
+    End Function
+
+    Public Sub writeErr(texte As String, classErr As String, functionErr As String)
+        Dim textFileStream As New IO.FileStream("C:\logiciel_gestion_5_mars_fichier\ErrLog\" + DateTime.Now.ToString("yyyy-MM-dd-H-m-s") + ".txt", IO.FileMode.OpenOrCreate, IO.FileAccess.ReadWrite, IO.FileShare.None)
+        Dim myFileWriter As New IO.StreamWriter(textFileStream)
+        myFileWriter.WriteLine(DateTime.Now.ToString())
+        myFileWriter.WriteLine("    Classe: " + classErr)
+        myFileWriter.WriteLine("    Function: " + functionErr)
+        myFileWriter.WriteLine("    Message d'erreur: " + texte)
+        myFileWriter.WriteLine("Fin du rapport d'erreure")
+        myFileWriter.WriteLine("")
+        myFileWriter.Close()
+        textFileStream.Close()
+    End Sub
+
+End Class
